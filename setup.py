dea451b5da6a688d616c197d7ac443f10c8e7407
Pedro Freire <pedrofreirex@gmail.com>
2020-06-18 18:27:10 -0700

    Add some of the requirements

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ from setuptools import setup,find_packages
 
 setup(
     name='rllab',
+    install_requires=[
+        'cached_property',
+        'pyprind',
+        'theano',
+    ],
     packages=[package for package in find_packages()
                 if package.startswith('rllab') or package.startswith('sandbox')],
     version='0.1.0',
